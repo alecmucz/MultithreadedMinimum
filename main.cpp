088e7162808ba3fa0d6753792b5020dea16285ed
amucz0 <alecmucz@gmail.com>
2024-10-10 20:55:27 -0400

    Clearer Terminal Output

--- main.cpp
+++ main.cpp
@@ -18,6 +18,10 @@ int main() {
 
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); ++i) {
         cout << "[" << arr[i] << "]";
+        if (i == ((sizeof(arr) / sizeof(arr[0]))/2)-1){
+            cout << " ";
+            continue;
+        }
     }
     cout << "\n";
 
